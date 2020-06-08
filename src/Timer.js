8c223678131b51e877ed53145c434dff7d5b2b6b
owenabbott <ocabbott@gmail.com>
2020-06-08 12:48:41 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -8,6 +8,15 @@ class Timer extends Component {
 
   // add your code here
 
+
+  componentDidMount(){
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+  
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
+
   render() {
     const { time, color } = this.state;
     return (
@@ -19,6 +28,8 @@ class Timer extends Component {
       </section>
     );
   }
+
+
 
   //clock functions
   clockTick = () => {
